c45a6e50a9513af5c9c07409fe88fe329e0c9bc9
Jacob Ebey <jacob.ebey@live.com>
2022-02-24 03:34:53 +0700

    fuck it, bundle it all

--- remix.config.js
+++ remix.config.js
@@ -5,9 +5,5 @@ module.exports = {
   server: "./server.ts",
   ignoredRouteFiles: [".*"],
   devServerBroadcastDelay: 1000,
-  serverDependenciesToBundle: [
-    "create-html-element",
-    "linkify-urls",
-    "stringify-attributes",
-  ],
+  serverDependenciesToBundle: [/.*/],
 };
